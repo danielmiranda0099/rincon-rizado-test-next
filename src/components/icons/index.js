0c188fef59f975c0495a6846c3909a42009c7abe
danielmiranda0099 <danielmesa877@gmail.com>
2024-05-01 21:27:31 -0500

    [ADD] Icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,2 +1,6 @@
 export * from './IconCheck';
 export * from './IconInstagram';
+export * from './IconFacebook';
+export * from './IconClock';
+export * from './IconPhone';
+export * from './IconMap';
