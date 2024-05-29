0418d998396f7afd3594f9c950838451e4257529
danielmiranda0099 <danielmesa877@gmail.com>
2024-05-29 14:31:58 -0500

    [UP] Carrousel Size Colunms

--- src/components/CardCarrouselSwiper.jsx
+++ src/components/CardCarrouselSwiper.jsx
@@ -16,27 +16,14 @@ export function CardCarrouselSwiper({ headerCarrousel, data }) {
       </div>
 
       <Swiper
-        slidesPerView={1.4}
+        slidesPerView= "auto"
         spaceBetween={30}
         navigation={true}
         pagination={{
           clickable: true,
         }}
         modules={[Navigation, Pagination]}
-        breakpoints={{
-          1500: {
-            slidesPerView: 5,
-            spaceBetween: 35,
-          },
-          990: {
-            slidesPerView: 4,
-            spaceBetween: 35,
-          },
-          590: {
-            slidesPerView: 2.5,
-            spaceBetween: 35,
-          },
-        }}
+        
         className="container-swiper-wrapper-card-services"
       >
         {
@@ -44,14 +31,16 @@ export function CardCarrouselSwiper({ headerCarrousel, data }) {
             <SwiperSlide
             className="container-card-services"
             tag="a"
-            style={{
-              backgroundImage: `url(${item.image})`,
-            }}
+            // style={{
+            //   backgroundImage: `url(${item.image})`,
+            // }}
             href={item.path}
             key={item.title}
           >
+            
+                <img src={item.image} alt="" />
+            
             <h3 className="card-services-title">{item.title}</h3>
-            <p className="card-services-content"></p>
           </SwiperSlide>
           ))
         }        
