9372abba077d28d8da411b0223f05621f79ffb2d
danielmiranda0099 <danielmesa877@gmail.com>
2024-06-04 16:34:09 -0500

    [UP ADD] Up Service Detail / Add New Gradients

--- src/app/services/[service]/page.jsx
+++ src/app/services/[service]/page.jsx
@@ -1,20 +1,26 @@
-'use server'
+"use client";
 import { IconCheck } from "@/components/icons";
 import "./styles.css";
 
 import { ALL_SERVICE } from "@/placeholders/services";
 import { SERVICE_DEFAULT } from "@/placeholders/services/default";
 
-export default async function ServisePage({params }) {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
-  let serviceSlug  = params.service;
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
-  let data = ALL_SERVICE.find( service => service.slug === serviceSlug);
+export default function ServisePage({ params }) {
+  let serviceSlug = params.service;
 
-  if(!data) data = SERVICE_DEFAULT
+  let data = ALL_SERVICE.find((service) => service.slug === serviceSlug);
 
-  console.log(data)
-  
+  if (!data) data = SERVICE_DEFAULT;
+
+  console.log(data);
+
   return (
     <section className="section-contained-full service-section-container">
       <div className="section-contained-md service-banner">
@@ -32,9 +38,7 @@ export default async function ServisePage({params }) {
             <span>{data.price}</span>
           </div>
 
-          <button className="btn btn-primary">
-            Contartarme para saber mas
-          </button>
+          <button className="btn btn-snow">Agendar Servicio</button>
         </div>
 
         <div className="service-banner-bg-content">
@@ -42,18 +46,48 @@ export default async function ServisePage({params }) {
         </div>
       </div>
 
-      <div className="section-contained-sm service-result-images">
+      {/* <div className="section-contained-sm service-result-images">
         <img src="https://images.pexels.com/photos/2576788/pexels-photo-2576788.jpeg" />
         <img src="https://images.pexels.com/photos/4668537/pexels-photo-4668537.jpeg" />
         <img src="/images/image-banner-products-favorite.jpg" />
         <img src="https://images.pexels.com/photos/2576788/pexels-photo-2576788.jpeg" />
-      </div>
+      </div> */}
+
+      <Swiper
+        tag="div"
+        slidesPerView="auto"
+        spaceBetween="16"
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination]}
+        className="section-contained-full service-result-images-container"
+      >
+        <SwiperSlide tag="div" className="service-result-images">
+          <img src="https://images.pexels.com/photos/2576788/pexels-photo-2576788.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide tag="div" className="service-result-images">
+          <img src="https://images.pexels.com/photos/4668537/pexels-photo-4668537.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide tag="div" className="service-result-images">
+          <img src="/images/image-banner-products-favorite.jpg" />
+        </SwiperSlide>
+        <SwiperSlide tag="div" className="service-result-images">
+          <img src="/images/image11.jpg" />
+        </SwiperSlide>
+        <SwiperSlide tag="div" className="service-result-images">
+          <img src="/images/image12.jpg" />
+        </SwiperSlide>
+      </Swiper>
 
       <div className="section-contained-sm service-content-offers">
-        <h2 className="text-lg text-center margin-bottom-mdx">El Servicio Incluye:</h2>
+        <h2 className="text-lg text-center margin-bottom-mdx">
+          El Servicio Incluye:
+        </h2>
         {data.service_includes.map((item) => (
           <div className="service-content-offers-item" key={item}>
-            <IconCheck /> <span>{item}</span>
+            <IconCheck size="2rem" color="#9287d2" /> <span>{item}</span>
           </div>
         ))}
       </div>
